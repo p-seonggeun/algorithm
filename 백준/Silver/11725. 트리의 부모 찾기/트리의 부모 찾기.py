39b6cbe4791049753c39de1df6cf1caed334afb2
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-01-25 21:25:39 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 416 ms, Memory: 80836 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+n = int(input())
+l = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
+dict = {}
+result = [[] for _ in range(n + 1)]
+for _ in range(n - 1) :
+    node1, node2 = map(int, input().split())
+    l[node1].append(node2)
+    l[node2].append(node1)
+
+def dfs(graph, v, visited) :
+    visited[v] = True
+    for i in graph[v] :
+        if not visited[i] :
+            dfs(graph, i, visited)
+        else :
+            dict[v] = i
+
+dfs(l, 1, visited)
+
+for i in range(2, n + 1) :
+    print(dict[i])
