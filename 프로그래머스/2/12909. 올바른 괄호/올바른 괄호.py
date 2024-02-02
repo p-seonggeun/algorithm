323b148f6ecba248df18f60181b295d96ba844da
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-02-02 20:26:24 +0900

    [level 2] Title: 올바른 괄호, Time: 13.76 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,19 @@
+def solution(s):
+    answer = True
+    l = []
+    for i in range(len(s)) :
+        if i == 0 :
+            if s[i] == ')' :
+                return False
+            else :
+                l.append(s[i])
+        else :
+            if s[i] == ')' :
+                if l :
+                    if l[-1] == '(' :
+                        l.pop()
+            else :
+                l.append(s[i])
+    if l :
+        return False
+    return True
