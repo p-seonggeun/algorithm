47910e2030e15b73c1e02c5c9041c9de96d2307d
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-02-14 11:37:13 +0900

    [Bronze II] Title: 소수, Time: 72 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.py
+++ 백준/Bronze/2581. 소수/소수.py
@@ -1,15 +1,18 @@
+from math import sqrt
 m = int(input())
 n = int(input())
 l = []
 
 for i in range(m, n + 1) :
+    if i == 1 :
+        continue
     count = 0
-    for j in range(1, i + 1) :
+    for j in range(2, int(sqrt(i)) + 1) :
         if i % j == 0 :
             count += 1
             if count > 2 :
                 break
-    if count == 2 :
+    if count == 0 :
         l.append(i)
 
 if l :
