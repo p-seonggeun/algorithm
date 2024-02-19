a170057c92b0d02bdd646afea6b8366fe5673389
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-02-19 18:33:57 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 174.45 ms, Memory: 23.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -0,0 +1,32 @@
+def solution(enroll, referral, seller, amount) :
+    answer = []
+    dict = {}
+    d = {"-" : 0}
+    for i in enroll :
+        if i not in d :
+            d[i] = 0
+            
+    for i, j in zip(enroll, referral) :
+        dict[i] = j
+    
+    amount = [i * 100 for i in amount]
+    
+    for i, j in zip(seller, amount) :
+        key, value = i, j
+        temp = 0
+        while key != '-':
+            if value == 0 :
+                break
+            next_value = int(value * 0.1)
+            current_value = value - next_value
+            value = next_value
+            d[key] += current_value
+            key = dict[key]
+            if key == '-' :
+                d[key] += value
+    
+    for i, j in d.items() :
+        if i != '-' :
+            answer.append(j)
+    
+    return answer
