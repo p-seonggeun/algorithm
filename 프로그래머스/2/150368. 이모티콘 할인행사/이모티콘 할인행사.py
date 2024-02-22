1426adc627fb53109c4cbc2a72fc3beb2729816b
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-02-22 14:20:12 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -6,24 +6,30 @@ def solution(users, emoticons):
     sale = [10, 20, 30, 40]
     sales = list(product(sale, repeat = len(emoticons)))
     l = []
+    for i in users :
+        i.append(0)
+        i.append(0)
         
     for i in sales :
+        copy_users = copy.deepcopy(users)
         e_plus, e_buy = 0, 0
-        for j in users :
-            u_s, u_m = j[0], j[1]
-            e, purchase = False, 0
-            for k, m in zip(i, emoticons) :
-                price = (m // 100 * (100 - k))
-                if k >= u_s :
-                    purchase += price
-                if purchase >= u_m :
-                    e = True
-                    break
-            if e :
+        
+        for j, k in zip(i, emoticons) :
+            price = (k // 100 * (100 - j))
+            
+            for m in copy_users :
+                if j >= m[0] :
+                    m[3] += price
+                    
+                if m[2] != 1 and m[3] >= m[1] :
+                    m[2] += 1
+                    m[3] = 0
+        
+        for m in copy_users :
+            if m[2] == 1 :
                 e_plus += 1
             else :
-                e_buy += purchase
-    
+                e_buy += m[3]
         l.append([e_plus, int(e_buy)])
     
     l.sort(key = lambda x : (x[0], x[1]), reverse = True)
