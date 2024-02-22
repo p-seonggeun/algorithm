ca1f36c44d798499482973b86f55dedf57291566
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-02-22 22:58:37 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = 0
+    for i in range(len(arr)) :
+        for j in range(len(arr[i])) :
+            if arr[i][j] == arr[j][i] :
+                answer = 1
+            else :
+                return 0
+        
+    return answer
