c8cf5c41a93cf2624932c2adb4813e829c57c6b1
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-04-04 17:08:39 +0900

    [Silver V] Title: 올림픽, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -15,8 +15,12 @@ for _ in range(N) :
 
 temp = list(score.items())
 temp.sort(key = lambda x : (-x[0][0], -x[0][1], -x[0][2]))
-
+answer = 0
 for index, i in enumerate(temp) :
     if K in i[1] :
-        print(index + 1)
+        answer += 1
         break
+    else :
+        answer += len(i[1])
+
+print(answer)
