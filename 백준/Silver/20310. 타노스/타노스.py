a59d782701ee0d91bcaa679271b0088cce3f4a6a
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-04-07 19:26:55 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/20310. 타노스/타노스.py
+++ 백준/Silver/20310. 타노스/타노스.py
@@ -1,5 +1,5 @@
-from itertools import permutations
-S = input().rstrip()
+S = list(input().rstrip())
+
 d = {}
 for i in S :
     if i in d :
@@ -7,9 +7,27 @@ for i in S :
     else :
         d[i] = 1
 
-d['0'] = d['0'] // 2
-d['1'] = d['1'] // 2
+for i in d :
+    d[i] = d[i] // 2
 
-l = ['0'] * d['0'] + ['1'] * d['1']
-l.sort()
-print(''.join(l))
+if '1' in d :
+    i = 0
+    while d['1'] > 0 :
+        if S[i] == '1' :
+            S[i] = '3'
+            d['1'] -= 1
+        i += 1
+
+if '0' in d :
+    i = len(S) - 1
+    while d['0'] > 0 :
+        if S[i] == '0' :
+            S[i] = '3'
+            d['0'] -= 1
+        i -= 1
+
+answer = ''
+for i in S :
+    if i != '3' :
+        answer += i
+print(answer)
