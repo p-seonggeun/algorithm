8de3067284c6d30d116fc91850943bbbdc987f40
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-04-15 12:24:38 +0900

    [Gold V] Title: 두 용액, Time: 244 ms, Memory: 41932 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+l = sorted(list(map(int, input().split())))
+
+s, e = 0, N - 1
+answer = [l[s], l[e]]
+m = abs(l[s] + l[e])
+flag = True # s를 왼쪽으로
+if abs(l[s]) < abs(l[e]) : flag = False # e를 오른쪽으로
+
+while s < e :
+    if s >= e or s + 1 == e or e - 1 == s :
+        break
+    if abs(l[s + 1] + l[e]) < abs(l[s] + l[e - 1]) :
+        s += 1
+    elif abs(l[s + 1] + l[e]) > abs(l[s] + l[e - 1]) :
+        e -= 1
+    else :
+        if flag : s += 1
+        else : e -= 1
+
+    if m >= abs(l[s] + l[e]) :
+        m = abs(l[s] + l[e])
+        answer = [l[s], l[e]]
+
+answer.sort()
+print(*answer)
