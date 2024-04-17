48c8d63979722706b7ac1f1e5d6cf6566182648f
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-04-17 11:02:07 +0900

    [D2] Title: 간단한 369게임, Time: 122 ms, Memory: 43,444 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -0,0 +1,22 @@
+#import sys
+#sys.stdin = open("input.txt", "r")
+
+N = int(input())
+
+for i in range(1, N + 1) :
+    flag = True
+    temp = ''
+    for j in str(i) :
+        if int(j) % 3 == 0 and j != '0' :
+            temp += '-'
+            flag = False
+    if i != N : 
+        if flag :
+            print(i, end = " ")
+        else :
+            print(temp, end = " ")
+    else :
+        if flag :
+            print(i)
+        else :
+            print(temp)
