4c0dd3a4e440861f77be1460c02c2a734fdea81e
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-04-18 16:53:58 +0900

    [Silver I] Title: IOIOI, Time: 380 ms, Memory: 34048 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,9 +3,18 @@ M = int(input())
 S = input().rstrip()
 
 oi = 'I' + ('OI' * N)
-answer = 0 
-for i in range(len(S) - len(oi) + 1) :
-    if S[i: i + len(oi)] == oi :
-        answer += 1
+ioi = 0
+answer = 0
+i = 0
+while i < len(S) - 2 :
+    if S[i] == 'I' and S[i + 1] == 'O' and S[i + 2] == 'I' :
+        ioi += 1
+        if ioi == N :
+            answer += 1
+            ioi -= 1
+        i += 1
+    else :
+        ioi = 0
+    i += 1
 
 print(answer)
