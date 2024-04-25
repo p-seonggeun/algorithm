a9bf72cbd7cf7015b7d50d6f88914dd42ccf70cd
p-seonggeun <143667221+p-seonggeun@users.noreply.github.com>
2024-04-25 16:21:35 +0900

    [Silver III] Title: Four Squares, Time: 212 ms, Memory: 126856 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,14 +1,13 @@
-N = int(input())
-def fs(n, depth, mini) :
-    root = int(n ** 0.5)
+n = int(input())
+first = {i * i for i in range(1, int(n ** 0.5) + 1)}
+second = {x + y for x in first for y in first}
+third = {x + y for x in first for y in second}
 
-    if root ** 2 == n :
-        return depth
-
-    for i in range(root, (root // (5 - depth)), -1) : 
-        x = fs(n - i ** 2, depth + 1, mini)
-        if mini > x :
-            mini = x
-    return mini
-
-print(fs(N, 1, 5))
+if n in first:
+    print(1)
+elif n in second:
+    print(2)
+elif n in third:
+    print(3)
+else:
+    print(4)
