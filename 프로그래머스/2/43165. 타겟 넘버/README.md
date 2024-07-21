90b5f2f1f913692403bc70a00cc2175591ad2fe5
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-07-21 18:16:47 +0900

    [level 2] Title: 타겟 넘버, Time: 1308.24 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,6 +1,6 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 18:16:05
+2024년 07월 21일 18:16:46
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,22 +1,3 @@
 def solution(numbers, target):
-    def dfs(index, count) :
-        global answer
-        if count == len(numbers) :
-            if sum(l) == target:
-                answer += 1
-            return
-
-        for i in range(len(sign)) :
-            l.append(int(sign[i] + numbers[count]))
-            dfs(i, count + 1)
-            l.pop()
-            
-    global answer        
-    answer = 0            
-    sign = ["+", "-"]
-    numbers = list(map(str, numbers))
-    l = []
-    dfs(0, 0)
-    
-    
+    answer = 0
     return answer
