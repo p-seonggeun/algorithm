41f03de0d609eec126faded3efbd684a2faa9b74
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-26 09:01:32 +0900

    [level 2] Title: 숫자 변환하기, Time: 1017.37 ms, Memory: 48.6 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -2,11 +2,12 @@ def solution(x, y, n):
     answer = 0
     INF = 1e9
     d = [INF] * (y + 1)
+    
     j = 0
     for i in range(x, y + 1, n) :
         d[i] = j
         j += 1
-
+        
     for i in range(x, y + 1) :
         if i - n >= 0 :
             d[i] = min(d[i], d[i - n] + 1)
@@ -17,7 +18,7 @@ def solution(x, y, n):
         if i % 6 == 0 :
             d[i] = min(d[i], d[i // 2] + 1, d[i // 3] + 1)
     
-    if d[y] == INF :
+    if d[y] >= INF :
         return -1
     else :
         return d[y]
