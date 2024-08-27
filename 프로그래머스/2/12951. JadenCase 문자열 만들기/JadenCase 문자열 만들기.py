68d0748cf0b51f138d2da8f3d2734c286b66262c
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-27 09:04:44 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,8 +1,7 @@
 def solution(s):
-    l = s.split(" ")
-    num = set([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-    for i in range(len(l)) :
-        if l[i] != "" :
-            if l[i][0] not in num :
-                l[i] = l[i].capitalize()
-    return ' '.join(l)
+    t = s.split(" ")
+    
+    for i in range(len(t)) :
+        t[i] = t[i].capitalize()
+
+    return " ".join(t)
