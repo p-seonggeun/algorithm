4080c48f7efb8aef20e5fad71ca777a4d5d0fcd8
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-27 09:12:57 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.50 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,21 +1,12 @@
 def solution(s):
-    answer = []
-    remove_count = 0
-    change_count = 0
-    
-    
+    a, b = 0, 0
     while s != '1' :
-        new_s = ""
-        for i in s :
-            if i == '0' :
-                remove_count += 1
-            else :
-                new_s += i
-    
-        c = len(new_s)
-        s = bin(c)[2:]
+        t = len(s)
+        s = s.replace("0", "")
+        b += t - len(s)
         
-        change_count += 1
-    answer.append(change_count)
-    answer.append(remove_count)
-    return answer
+        s = bin(len(s))[2:]
+        
+        a += 1
+    
+    return [a, b]
