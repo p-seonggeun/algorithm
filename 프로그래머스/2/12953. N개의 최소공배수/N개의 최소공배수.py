3825bdc6766f7f8b8bf7fe18d1020399ccd0e4df
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-28 08:40:51 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,12 @@
+import math
+def lcm(a, b) :
+    
+    return (a * b) // math.gcd(a, b)
+
+def solution(arr):
+    answer = 0
+    
+    answer = arr[0]
+    for i in range(1, len(arr)) :
+        answer = lcm(answer, arr[i])
+    return answer
