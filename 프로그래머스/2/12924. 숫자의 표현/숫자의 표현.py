fa6175d6f672b8903d6b551a7c2f4b6e3df18e90
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-27 17:45:04 +0900

    [level 2] Title: 숫자의 표현, Time: 6.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,14 @@
 def solution(n):
     answer = 0
-    l = [i + 1 for i in range(n)]
     
-    for i in range(n) :
-        temp = 0
-        for j in range(i, n) :
-            if temp == n :
+    for i in range(1, n + 1) :
+        t = 0
+        for j in range(i, n + 1) :
+            t += j
+            if t > n :
+                break
+            elif t == n :
+                t = 0
                 answer += 1
                 break
-            elif temp > n :
-                break
-            temp += l[j]
-    return answer + 1
+    return answer
