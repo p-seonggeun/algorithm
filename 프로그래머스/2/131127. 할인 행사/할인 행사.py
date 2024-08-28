10723b8593f6752bc5dd40e33066d00a4b714b32
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-28 13:45:14 +0900

    [level 2] Title: 할인 행사, Time: 159.97 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -1,23 +1,16 @@
-import copy
 def solution(want, number, discount):
     answer = 0
     d = {}
     for i, j in zip(want, number) :
         d[i] = j
     
-    for i in range(len(discount) - 9) :
-        dict = copy.deepcopy(d)   
-        for j in range(10) :
-            if discount[i + j] not in dict :
-                continue
-            dict[discount[i + j]] -= 1
-        temp = dict.values()
-        flag = True
-        for i in temp :
-            if i > 0 :
-                flag = False
-                break
-        if flag :
-            answer += 1
+    for i in range(len(discount) - 10 + 1) :
+        t = {}
+        for j in range(i, i + 10) :
+            if discount[j] in t :
+                t[discount[j]] += 1
+            else : t[discount[j]] = 1
         
+        if d == t : answer += 1
+    
     return answer
