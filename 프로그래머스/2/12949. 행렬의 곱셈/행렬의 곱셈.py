cb87fc024b45ed3516a4fe0f824860317fd63196
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-28 14:39:45 +0900

    [level 2] Title: 행렬의 곱셈, Time: 95.73 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,14 +1,13 @@
 def solution(arr1, arr2):
     answer = []
-    n, m = len(arr1), len(arr1[0])
-    
-    for i in range(n) :
-        temp = []
-        for j in range(len(arr2[0])) :
+
+    for i in range(len(arr1)) :
+        t = []
+        for j in range(len(arr2[0])) : # arr2 열만큼
             s = 0
-            for k in range(len(arr2)) :
-                s += (arr1[i][k] * arr2[k][j])
-            temp.append(s)
-        answer.append(temp)
+            for k in range(len(arr2)) : # arr2 행
+                s += arr1[i][k] * arr2[k][j]
+            t.append(s)
+        answer.append(t)
     
     return answer
