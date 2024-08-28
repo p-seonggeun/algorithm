040fb4f7beb804a7157149e1e2104747979b0d0d
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-28 14:48:48 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,35 +1,14 @@
-from itertools import combinations
-
 def solution(clothes):
-    answer = 0
-    dict = {}
+    answer = 1
+    d = {}
+    
     for i in clothes :
-        if i[1] in dict :
-            dict[i[1]].append(i[0])
+        if i[1] in d :
+            d[i[1]].append(i[0])
         else :
-            dict[i[1]] = [i[0]]
-            
-    dk = list(dict.keys())
-    dv = list(dict.values())
-    flag = True
+            d[i[1]] = [i[0]]
     
-    for i in dv :
-        if len(i) != 1 :
-            flag = False
-            break
-            
-    if flag :
-        return 2 ** len(dk) - 1
-    
-    temp = []
-    for i in range(1, len(dk) + 1) :
-        for j in (list(combinations(dk, i))) :
-            temp.append(j)
+    for i in d :
+        answer *= len(d[i]) + 1
 
-    for i in temp :
-        t = 1
-        for j in i :
-            t *= (len(dict[j]))
-        answer += t
-    
-    return answer
+    return answer - 1
