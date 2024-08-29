52e98299b955ce8f0dcc6eee425b6d81a5c76988
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-29 17:17:45 +0900

    [level 2] Title: 기능개발, Time: 0.19 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,24 +1,27 @@
 def solution(progresses, speeds):
     answer = []
-    temp = []
+    t = []
+    
     for i, j in zip(progresses, speeds) :
-        cnt = 0
+        c = 0
         while i < 100 :
             i += j
-            cnt += 1
-        temp.append(cnt)
-        
-    l = []
+            c += 1
+        t.append(c)
     
-    for i in range(len(temp)) :
-        num = 1
-        if i in l :
-            continue
-        for j in range(i + 1, len(temp)) :
-            if temp[i] >= temp[j] :
-                num += 1
-                l.append(j)
-            else :
+    temp = []
+    for i in range(len(t)) :
+        if i in temp : continue
+        c = 0
+        for j in range(i, len(t)) :
+            if t[j] > t[i] :
+                answer.append(c)
                 break
-        answer.append(num)
+            else :
+                c += 1
+                temp.append(j)
+        else :
+            answer.append(c)
+            
+            
     return answer
