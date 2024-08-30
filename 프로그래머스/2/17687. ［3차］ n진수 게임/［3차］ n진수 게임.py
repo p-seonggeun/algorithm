4e73e393c6e6dfe0bb08761cdeae3b995bc4154b
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-30 21:08:40 +0900

    [level 2] Title: [3차] n진수 게임, Time: 197.36 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -1,26 +1,30 @@
-dictionary = {10 : 'A', 11 : 'B', 12 : 'C', 13 : 'D', 14 : 'E', 15 : 'F'}
-def change(num, n) :
-    s = ''
-    if num == 0 :
-        return '0'
-    while num >= 1 :
-        if 10 <= num % n <= 15 :
-            s += dictionary[num% n]
+d = {10 : 'A', 11 : 'B', 12 : 'C', 13 : 'D', 14 : 'E', 15 : 'F'}
+def change(number, n) :
+    if number == 0 :
+        return "0"
+    temp = []
+    
+    while number > 1 :
+        if 10 <= number % n <= 15 :
+            temp.append(d[number % n])
         else :
-            s += str(num % n)
-        num = num // n
-    return s[::-1]
+            temp.append(str(number % n))
+        number //= n
+    
+    if number != 0 :
+        temp.append(str(number))
+    return ''.join(temp[::-1])
 
 def solution(n, t, m, p):
     answer = ''
+    
     temp = ''
-    for i in range(0, t * m + 1) :
+    for i in range(t * m) :
         temp += change(i, n)
     
     for i in range(p - 1, len(temp), m) :
+        answer += temp[i]
         if len(answer) == t :
             break
-        answer += temp[i]
-        
     
     return answer
