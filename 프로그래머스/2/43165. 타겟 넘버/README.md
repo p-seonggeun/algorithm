c199deca72196d8dd946cd7b53d4c1851677049a
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-30 20:33:59 +0900

    [level 2] Title: 타겟 넘버, Time: 1627.62 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 1308.24 ms
+메모리: 10.5 MB, 시간: 1627.62 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 18:16:46
+2024년 08월 30일 20:33:57
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,38 @@
+sign = ["+", "-"]
+l = []
+
+def check(t) :
+    global target_num
+    if sum(t) == target_num :
+        return True
+    else : return False
+
+def dfs(count) :
+    global limit
+    global answer
+    global list_numbers
+    if count == limit :
+        if check(l) :
+            answer += 1
+        return
+    
+    for i in range(len(sign)) :
+        l.append(int(sign[i] + str(list_numbers[count])))
+        dfs(count + 1)
+        l.pop()
+    
 def solution(numbers, target):
+    global limit
+    global answer
+    global list_numbers
+    list_numbers = numbers
+    global target_num
+    target_num = target
+    
+    limit = len(numbers)
     answer = 0
+    
+    dfs(0)
+    
+    
     return answer
