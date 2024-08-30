b7a9ff75df996785af923336dc1acb51659d066d
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-08-30 20:09:14 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.97 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,22 +1,57 @@
-from collections import Counter
 def solution(str1, str2):
     answer = 0
-    a, b = [], []
+    l1, l2 = [], []
+    
     for i in range(len(str1) - 1) :
-        temp = str1[i : i + 2]
-        if (temp.isalpha()):
-            a.append(temp.upper())
-        
+        t = str1[i : i + 2]
+        if t.isalpha() :
+            l1.append(t.lower())
+    
     for i in range(len(str2) - 1) :
-        temp = str2[i : i + 2]
-        if temp.isalpha() :
-            b.append(temp.upper())
+        t = str2[i : i + 2]
+        if t.isalpha() :
+            l2.append(t.lower())
     
-    a = Counter(a)
-    b = Counter(b)
+    d1, d2 = {}, {}
+    for i in l1 :
+        if i in d1 :
+            d1[i] += 1
+        else : d1[i] = 1
     
-    if sum((a | b).values()) == 0 :
+    for i in l2 :
+        if i in d2 :
+            d2[i] += 1
+        else : d2[i] = 1
+    
+    print(d1, d2)
+    s = set()
+    for i in l1 :
+        if i not in s :
+            s.add(i)
+    
+    for i in l2 :
+        if i not in s :
+            s.add(i)
+    
+    union, intersection = [], []
+    for i in s :
+        if i in d1 and i in d2 :
+            for _ in range(min(d1[i], d2[i])) :
+                intersection.append(i)
+            for _ in range(max(d1[i], d2[i])) :
+                union.append(i)
+        else :
+            if i in d1 :
+                for _ in range(d1[i]) :
+                    union.append(i)
+            else :
+                for _ in range(d2[i]) :
+                    union.append(i)
+    
+    if intersection and union :
+        answer = int((len(intersection) / len(union)) * 65536)
+    elif intersection or (intersection == union):
         return 65536
-    answer = int((sum((a & b).values()) / sum((a | b).values())) * 65536)
+    else :
+        return 0
     return answer
-    
