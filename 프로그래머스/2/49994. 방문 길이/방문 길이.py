a44770aa3ea941df1aaa2bb27e39abe74de57531
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-09-01 15:34:06 +0900

    [level 2] Title: 방문 길이, Time: 0.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,26 +1,33 @@
+d = {'U' : [-1, 0], 'D' : [1, 0], 'L' : [0, -1], 'R' : [0, 1]}
 from collections import deque
-def solution(dirs):
-    answer = 0
-    d = { "U" : [-1, 0], "D" : [1, 0], "L" : [0, -1], "R" : [0, 1] }
-    board = [[0 for _ in range(11)] for _ in range(11)]
-    s = set()
-    pos = [5, 5]
+def bfs(dirs, s) :
+    global answer
+    queue = deque()
+    queue.append((5, 5))
     
     for i in dirs :
+        x, y = queue.popleft()
         
-        nx = pos[0] + d[i][0]
-        ny = pos[1] + d[i][1]
+        nx = x + d[i][0]
+        ny = y + d[i][1]
         
         if 0 <= nx < 11 and 0 <= ny < 11 :
-            if (nx, ny, pos[0], pos[1]) and (pos[0], pos[1], nx, ny) not in s :
+            if (x, y, nx, ny) not in s :
+                s.add((x, y, nx, ny))
+                s.add((nx, ny, x, y))
                 answer += 1
-                s.add((nx, ny, pos[0], pos[1]))
-                s.add((pos[0], pos[1], nx, ny))
-            pos[0] = nx
-            pos[1] = ny
-            board[nx][ny] += 1
-            
-            
-    for i in board :
-        print(i)
+                queue.append((nx, ny))
+            else :
+                queue.append((nx, ny))
+        else :
+            queue.append((x, y))
+
+def solution(dirs):
+    global answer
+    answer = 0
+    board = [[0 for _ in range(11)] for _ in range(11)]
+    s = set()
+    bfs(dirs, s)
+    
+    
     return answer
