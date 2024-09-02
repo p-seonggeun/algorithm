ed4911711b6004b7a895c99bda159d811845b84e
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-09-02 20:14:38 +0900

    [level 2] Title: 땅따먹기, Time: 225.57 ms, Memory: 43.2 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,17 +1,17 @@
 def solution(land):
     answer = 0
-    N = len(land)
-    for i in range(1, N) :
-        for j in range(4) :
-            if j == 0 :
-                land[i][j] += max(land[i - 1][j + 1], land[i - 1][j + 2], land[i - 1][j + 3])
-            elif j == 1 :
-                land[i][j] += max(land[i - 1][j - 1], land[i - 1][j + 1], land[i - 1][j + 2])
-            elif j == 2 :
-                land[i][j] += max(land[i - 1][j - 2], land[i - 1][j - 1], land[i - 1][j + 1])
-            elif j == 3 :
-                land[i][j] += max(land[i - 1][j - 3], land[i - 1][j - 2], land[i - 1][j - 1])
     
-    answer = max(land[-1])
+    d = [[0 for _ in range(4)] for _ in range(len(land))]
+    d[0][0] = land[0][0]
+    d[0][1] = land[0][1]
+    d[0][2] = land[0][2]
+    d[0][3] = land[0][3]
     
+    for i in range(1, len(land)) :
+        d[i][0] = max(d[i - 1][1], d[i - 1][2], d[i - 1][3]) + land[i][0]
+        d[i][1] = max(d[i - 1][0], d[i - 1][2], d[i - 1][3]) + land[i][1]
+        d[i][2] = max(d[i - 1][0], d[i - 1][1], d[i - 1][3]) + land[i][2]
+        d[i][3] = max(d[i - 1][0], d[i - 1][1], d[i - 1][2]) + land[i][3]
+    
+    answer = max(d[len(land) - 1])
     return answer
