86916688a8d77ebe66e7f2e84f075ac0a547e154
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-09-02 20:35:09 +0900

    [level 2] Title: 주식가격, Time: 61.93 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,10 +1,22 @@
 def solution(prices):
     answer = []
+    stack = []
+    temp = []
     
-    for i in range(len(prices)) :
-        for j in range(i + 1, len(prices)) :
-            if prices[i] > prices[j] :
-                break
-        answer.append(j - i)
+    for index, i in enumerate(prices) :
+        
+        while stack and stack[-1][-1] > i :
+            t = stack.pop()
+            temp.append((t[0], index - t[0]))
+        stack.append((index, i))
+    
+    else :
+        while stack :
+            t = stack.pop()
+            temp.append((t[0], index - t[0]))
+        
+    temp.sort(key = lambda x : x[0])
+    for i in temp :
+        answer.append(i[1])
         
     return answer
