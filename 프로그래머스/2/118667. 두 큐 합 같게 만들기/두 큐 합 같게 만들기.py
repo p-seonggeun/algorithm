2e5745391442b0a24d9568cc429556e1f04e6519
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-09-04 16:46:43 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 136.28 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,31 +1,30 @@
 from collections import deque
 def solution(queue1, queue2):
-    count = 0
+    answer = 0
     queue1 = deque(queue1)
     queue2 = deque(queue2)
-    sum_queue1 = sum(queue1)
-    sum_queue2 = sum(queue2)
-    max_count = (len(queue1) + len(queue2)) * 2
+    count = (len(queue1) + len(queue2)) * 2 
+    s_q1 = sum(queue1)
+    s_q2 = sum(queue2)
     
-    if (sum_queue1 + sum_queue2) % 2 == 1 :
-        return 1
     
-    while sum_queue1 != sum_queue2 :
-        if count >= max_count :
-            return -1
+    while queue1 and queue2 and s_q1 != s_q2 :
+        if answer >= count : return -1
+        while queue1 and s_q1 > s_q2 :
+            t = queue1.popleft()
+            s_q1 -= t
+            s_q2 += t
+            queue2.append(t)
+            answer += 1
         
-        while queue1 and sum_queue1 > sum_queue2 :
-            temp = queue1.popleft()
-            queue2.append(temp)
-            count += 1
-            sum_queue1 -= temp
-            sum_queue2 += temp
-            
-        while queue2 and sum_queue2 > sum_queue1 :
-                temp = queue2.popleft()
-                queue1.append(temp)
-                count += 1
-                sum_queue2 -= temp
-                sum_queue1 += temp
-                
-    return count
+        while queue2 and s_q1 < s_q2 :
+            t = queue2.popleft()
+            s_q2 -= t
+            s_q1 += t
+            answer += 1
+            queue1.append(t)
+    
+    if queue1 and queue2 :
+        return answer
+    else :
+        return -1
