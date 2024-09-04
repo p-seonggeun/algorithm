4d28db463aa34a4505818b53ffd816b2e42a6684
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-09-04 16:58:19 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 279.21 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,18 +1,16 @@
-def solution(bridge_length, weight, truck_weights):
+def solution(bridge_length, weight, truck_weights) :
     answer = 0
+    queue = [0] * bridge_length
+    s = 0
     
-    q = [0] * bridge_length
-    s = sum(q)
-    time = 0
-    
-    while q :
-        s -= q.pop(0)
+    while queue :
+        s -= queue.pop(0)
         if truck_weights :
             if s + truck_weights[0] <= weight :
-                s = s + truck_weights[0]
-                q.append(truck_weights.pop(0))
+                s += truck_weights[0]
+                queue.append(truck_weights.pop(0))
             else :
-                q.append(0)
-        time += 1
-    answer = time
+                queue.append(0)
+        answer += 1
+        
     return answer
