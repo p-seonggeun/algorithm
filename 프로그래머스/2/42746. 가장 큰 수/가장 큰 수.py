073eac4ac7de1e3773544d2c3d5a4f0a2e1aca4b
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-09-04 15:33:29 +0900

    [level 2] Title: 가장 큰 수, Time: 92.15 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,10 +1,9 @@
 def solution(numbers):
-    numbers = [str(i) for i in numbers]
+    answer = ''
+    numbers = list(map(str, numbers))
+    
     numbers.sort(key = lambda x : (x * 4)[:4], reverse = True)
     
     answer = ''.join(numbers)
-    
-    if answer[0] == '0':
-        return '0'
-    else:
-        return answer
+    if answer[0] == '0' : return '0'
+    return answer
