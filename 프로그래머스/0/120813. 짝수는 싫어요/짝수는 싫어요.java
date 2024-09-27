68abbc8e0bbc8c48132389f271b473e481c7a383
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-09-27 10:15:38 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.96 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                result.add(i);
+            }
+        }
+        
+        int[] answer = result
+            .stream()
+            .mapToInt(i -> i)
+            .toArray();
+        
+        return answer;
+    }
+}
