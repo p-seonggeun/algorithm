6d7a0a25a4b4c413e701323ebb250eb06448d9ad
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-10-07 17:51:36 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.06 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            sb.append(my_string.charAt(i));
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
