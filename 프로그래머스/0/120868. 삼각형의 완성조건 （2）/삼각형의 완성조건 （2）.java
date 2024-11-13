572852c00e455953180815f8af3c73d89f635611
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2024-11-13 09:11:43 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.57 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+        
+        Arrays.sort(sides);
+
+        for (int i = sides[1] + 1; i < sides[0] + sides[1]; i++) {
+            answer++;
+        }
+
+        for (int i = 1; i <= sides[1]; i++) {
+            if (i + sides[0] > sides[1]) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
