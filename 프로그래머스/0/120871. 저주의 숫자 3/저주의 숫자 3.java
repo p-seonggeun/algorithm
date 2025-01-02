c7d6035e3421b823b4ce58c7d3eb2fd87e6920b0
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2025-01-02 14:35:15 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.28 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static int solution(int n) {
+        int[] dp = new int[101];
+
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (dp[i] % 3 == 0 || String.valueOf(dp[i]).contains("3")) {
+                while (dp[i] % 3 == 0 || String.valueOf(dp[i]).contains("3")) {
+                    dp[i] += 1;
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+}
