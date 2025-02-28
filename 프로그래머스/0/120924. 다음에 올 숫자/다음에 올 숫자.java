75d1f770bcb77bc988466e9ca0da9b115c05bb99
Seong Geun Park <143667221+p-seonggeun@users.noreply.github.com>
2025-02-28 09:59:32 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        if (common[0] - common[1] == common[1] - common[2]) {
+            return common[common.length - 1] + (common[1] - common[0]);
+        }
+        return common[common.length - 1] * (common[1] / common[0]);
+    }
+}
