645d51e0be70cd7a247cfffab270319dc44aa00e
White <143667221+p-seonggeun@users.noreply.github.com>
2025-07-04 14:52:33 +0900

    [Silver IV] Title: 이삿짐센터, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16237. 이삿짐센터/이삿짐센터.py
+++ 백준/Silver/16237. 이삿짐센터/이삿짐센터.py
@@ -1,34 +1,34 @@
-A,B,C,D,E=map(int,input().split())
-answer=0
-answer+=E
+A, B, C, D, E = map(int, input().split())
+answer = 0
+answer += E
 
-while D>0:
-    if A>0:
-        A-=1
-    D-=1
-    answer+=1
+while D > 0:
+    if A > 0:
+        A -= 1
+    D -= 1
+    answer += 1
 
-while C>0:
-    if B>0:
-        B-=1
-    elif A>0:
-        A-=2
-    C-=1
-    answer+=1
+while C > 0:
+    if B > 0:
+        B -= 1
+    elif A > 0:
+        A -= 2
+    C -= 1
+    answer += 1
 
-while B>0:
-    if B>=2 and A>=1:
-        B-=2
-        A-=1
-    elif B>=1 and A>=1:
-        B-=1
-        A-=3
+while B > 0:
+    if B >= 2 and A >= 1:
+        B -= 2
+        A -= 1
+    elif B >= 1 and A >= 1:
+        B -= 1
+        A -= 3
     else:
-        B-=2
-    answer+=1
+        B -= 2
+    answer += 1
 
-while A>0:
-    A-=5
-    answer+=1
+while A > 0:
+    A -= 5
+    answer += 1
 
 print(answer)
