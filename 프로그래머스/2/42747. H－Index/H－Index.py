8353da3499f9648800004c589f1364de52a5de00
White <143667221+p-seonggeun@users.noreply.github.com>
2025-08-25 13:40:53 +0900

    [level 2] Title: H-Index, Time: 47.63 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,15 @@
 def solution(citations):
     answer = 0
-    citations.sort(reverse = True)
     
-    for index, i in enumerate(citations) :
-        if i <= index :
-            return index
-    
-    return len(citations)
+    while True:
+        a = []
+        b = []
+        for i in citations:
+            if i >= answer:
+                a.append(i)
+            else:
+                b.append(i)
+        
+        if len(a) < answer:
+            return answer - 1
+        answer += 1
