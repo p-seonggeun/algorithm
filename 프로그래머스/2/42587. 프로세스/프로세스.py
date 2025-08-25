491eca1ebc3e411cf88ba2ece5b07469cd73da33
White <143667221+p-seonggeun@users.noreply.github.com>
2025-08-25 14:25:09 +0900

    [level 2] Title: 프로세스, Time: 0.16 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,29 +1,23 @@
 from collections import deque
 def solution(priorities, location):
     answer = 0
-    l = []
     
-    stand = []
-    for index, i in enumerate(priorities) :
-        l.append([i, index])
-        if index == location :
-            stand = [i, index]
+    a = sorted(priorities)
     
+    l = deque()
+    for index, i in enumerate(priorities):
+        l.append([index, i])
     
-    priorities.sort(reverse = True)
-    
-    queue_p = deque(priorities)
-    queue = deque(l)
-    count = 0
-    
-    while True :
-        t = queue.popleft()
-        if t[0] == queue_p[0] :
-            queue_p.popleft()
-            count += 1
-            if t[1] == stand[1] :
-                break
-        else :
-            queue.append(t)
-    
-    return count
+    m = a.pop()
+    while m and l:
+        t = l.popleft()
+        
+        if m <= t[1]:
+            answer += 1
+            if location == t[0]:
+                return answer
+            m = a.pop()
+        else:
+            l.append(t)
+            
+    return answer
