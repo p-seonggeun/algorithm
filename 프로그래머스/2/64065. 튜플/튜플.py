ca89247ed32d095bab40572f107f636b30fb0794
박성근 <143667221+p-seonggeun@users.noreply.github.com>
2025-11-24 09:08:11 +0900

    [level 2] Title: 튜플, Time: 313.54 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,26 +1,12 @@
 def solution(s):
     answer = []
-    s = s[1:len(s) - 1]
-
-    l = []
-    t = []
+    s = s[2:-2]
+    temp = s.split("},{")
+    temp.sort(key = lambda x : len(x))
     
-    for index, i in enumerate(s):
-        if i == '{':
-            temp = ''
-            t = []
-        elif i == ',' and s[index + 1].isnumeric():
-            t.append(int(temp))
-            temp = ''
-        elif i == '}':
-            t.append(int(temp))
-            l.append(t)
-        elif i.isnumeric():
-            temp += i
-    
-    l.sort(key = lambda x : len(x))
-    for i in l:
-        for j in i:
+    for i in temp:
+        for j in i.split(","):
+            j = int(j)
             if j not in answer:
                 answer.append(j)
     return answer
